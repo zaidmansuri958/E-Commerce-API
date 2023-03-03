858afd83617c2de9a2b3b6534fea44c86bea5ff8
zaidmansuri958 <firozam196@gmail.com>
2023-03-04 00:19:14 +0530

    E commerce

--- src/Controllers/maleShoeController.js
+++ src/Controllers/maleShoeController.js
@@ -4,7 +4,7 @@ const ShoeModel = require("../Models/maleShoes");
 const getProduct = async function (req, res) {
     try {
         const maleShoes = await ShoeModel.find();
-        res.status(200).json(maleClothes);
+        res.status(200).json(maleShoes);
     } catch (error) {
         console.log(error);
         res.status(500).json({message :"Something wrong happen"});
