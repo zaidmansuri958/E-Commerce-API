d6698de1f07c8d19622e2e94ba3ad07e51c98b7e
zaidmansuri958 <firozam196@gmail.com>
2023-03-07 20:38:50 +0530

    Category list changed

--- src/Controllers/categoryController.js
+++ src/Controllers/categoryController.js
@@ -4,7 +4,7 @@ const CategoryModel = require("../Models/category");
 const getProduct = async function (req, res) {
     try {
         const category = await CategoryModel.find();
-        res.status(200).json(category);
+        res.status(200).json({Category:category});
     } catch (error) {
         console.log(error);
         res.status(500).json({message :"Something wrong happen"});
