a3b34f601a10fe94e3101f9b17e96ef3862e6011
zaidmansuri958 <firozam196@gmail.com>
2023-03-30 14:29:15 +0530

    Like Feature added

--- src/Controllers/userLikedController.js
+++ src/Controllers/userLikedController.js
@@ -21,7 +21,7 @@ const createLiked = async function (req, res) {
         res.status(201).json(newLiked)
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: $error });
+        res.status(500).json({ message: "something went wrong" });
     }
 }
 
